f913dd06d6b2521d20fa3ed4f56781116df10042
euoluizg <gmelo17177@gmail.com>
2025-11-17 09:05:14 -0300

    Config: Preparação para deploy no Render (Gunicorn e DB URL)

--- API/app/config/db.py
+++ API/app/config/db.py
@@ -2,11 +2,21 @@ import psycopg2
 import os
 
 def createConnection():
-    conn = psycopg2.connect(
-        dbname=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        host=os.getenv("DB_HOST"),
-        port=os.getenv("DB_PORT")
-    )
-    return conn
+    # Cria uma conexão com o barco de dados (Local ou nuvem).
+
+    try:
+        database_url = os.getenv("DATABE_URL")
+        if database_url:
+            conn = psycopg2.connect(database_url)
+        else:
+            conn = psycopg2.connect(
+            dbname=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            host=os.getenv("DB_HOST"),
+            port=os.getenv("DB_PORT")
+        )
+        return conn
+    except Exception as e:
+        print(f"Erro ao conectar ao DB: {e}")
+        return None
